f2ed09925dbf12364b6662a5f84a4b259ae4d0d1
Santiago Forero Arevalo <ds.foreroa@uniandes.edu.co>
2022-02-01 14:51:01 -0500

    firebase setup

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -8,15 +8,8 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Column(
+        children: [Text('Bienvenido!'), TextField(), TextField()],
       ),
     );
   }
