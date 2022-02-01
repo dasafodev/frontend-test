6d3d4636050c75c46d29f2169eb40f3ec659f88a
Santiago Forero Arevalo <ds.foreroa@uniandes.edu.co>
2022-02-01 17:19:04 -0500

    signup module removed

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -6,8 +6,6 @@ import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
-import '../modules/sign_up/bindings/sign_up_binding.dart';
-import '../modules/sign_up/views/sign_up_view.dart';
 
 part 'app_routes.dart';
 
@@ -26,11 +24,6 @@ class AppPages {
       name: _Paths.LOGIN,
       page: () => LoginView(),
       binding: LoginBinding(),
-    ),
-    GetPage(
-      name: _Paths.SIGN_UP,
-      page: () => SignUpView(),
-      binding: SignUpBinding(),
     ),
     GetPage(
       name: _Paths.HISTORY,
